b439ced3b653d8a47dc61c2293371db114fbe67a
ganeshk2014 <35740048+ganeshk2014@users.noreply.github.com>
2020-03-19 21:03:16 +0530

    Add files via upload

---
+++ tod.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from flask import Flask
+from datetime import datetime
+app = Flask(__name__)
+@app.route('/default')
+def default():
+    hour = datetime.now().hour
+    return (
+        "2" if 5 <= hour <= 11
+        else
+        "0" if 12 <= hour <= 17
+        else
+        "1" if 18 <= hour <= 24
+    )
+if __name__ =='__main__':
+    app.run(debug=True)
+
